060213bdd807965a0bf66d1d139d6936abf94bdf
Matteo Parrucci <parruc@gmail.com>
2016-09-18 20:18:24 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='parruc.imagefilters',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="This package allows to apply B/W filter to scaled images",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
